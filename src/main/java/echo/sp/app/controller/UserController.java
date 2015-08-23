136ac804b64538c6280577fb584ad685446d4bcb
EthanProg <programcccethan@gmail.com>
2015-08-23 14:54:48 +0800

    TTTTT

--- src/main/java/echo/sp/app/controller/UserController.java
+++ src/main/java/echo/sp/app/controller/UserController.java
@@ -1,52 +1,38 @@
 package echo.sp.app.controller;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import echo.sp.app.model.User;
+import echo.sp.app.command.core.CoreController;
+import echo.sp.app.command.model.Code;
 import echo.sp.app.service.UserService;
 
-
 @Controller
-public class UserController {
+public class UserController extends CoreController{
+	
 	@Autowired
 	private UserService userService;
 	
 	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 	
-	@RequestMapping("index")
-    public String index(Model model){
-        User user = new User();
-        user.setNickname("jingwen");
-        user.setState(2);
-        userService.insertUser(user);
-        
-        logger.info("sssss - " + user);
-        
-        model.addAttribute("serverTime", "jingwen");
-        return "index";
-    }
-	
-	@RequestMapping("search")
-    public String search(HttpServletRequest req,HttpServletResponse response){
-        Map<String, Object> parmMap = new HashMap<String, Object>();
-        parmMap.put("page", Integer.valueOf(req.getParameter("page")));
-        parmMap.put("limit", Integer.valueOf(req.getParameter("limit")));
-        logger.debug("parm : " + parmMap);
-        List list = userService.getData(parmMap);
-        logger.debug("List : " + list);
-        req.setAttribute("ddd", list.toString());
-        return "search";
-    }
+	@RequestMapping("checkReg")
+	public void checkReg(HttpServletResponse response, @RequestParam String tel) {
+		if (!"".equals(tel)) {
+			Map parmMap = new HashMap();
+			parmMap.put("TEL", tel);
+			int res = userService.getCheckReg(parmMap);
+			parmMap = new HashMap();
+			parmMap.put("IS_EXIST", res > 0 ? "1" : "0");
+			super.writeJson(response, Code.SUCCESS, Code.SUCCESS_MSG, parmMap, null);
+		}
+	}
 }
