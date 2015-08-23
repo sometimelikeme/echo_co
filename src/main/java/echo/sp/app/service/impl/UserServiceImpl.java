ace66d2ed2e7cf9cb9d03760e25afecaa7b05aee
EthanProg <programcccethan@gmail.com>
2015-08-23 09:28:04 +0800

    测试用例2

--- src/main/java/echo/sp/app/service/impl/UserServiceImpl.java
+++ src/main/java/echo/sp/app/service/impl/UserServiceImpl.java
@@ -3,8 +3,11 @@ package echo.sp.app.service.impl;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import echo.sp.app.dao.UserDAO;
 import echo.sp.app.model.User;
@@ -12,12 +15,28 @@ import echo.sp.app.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService{
+	private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 	@Autowired
     private UserDAO userDAO;
      
     @Override
     public int insertUser(User user) {
-        return userDAO.insertUser(user);
+    	logger.debug("ggg---insertUser---begin-ttt");
+    	user.setId(4);
+    	userDAO.insertUser1(user);
+    	logger.debug("ggg---insertUser---prase1");
+    	User user1 = new User();
+    	user1.setNickname("jingwen");
+    	user1.setState(3);
+    	user1.setId(5);
+    	userDAO.insertUser1(user1);
+    	/*try {
+    		userDAO.insertUser1(user1);
+		} catch (Exception e) {
+			logger.debug("ggg---insertUser---prase2",e);
+		}*/
+    	logger.debug("ggg---insertUser---prase2");
+        return 1;
     }
     
     @Override
