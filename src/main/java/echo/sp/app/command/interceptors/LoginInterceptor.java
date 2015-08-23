a8aa811595bb3cce80451fc79108d3c151abc7c3
EthanProg <programcccethan@gmail.com>
2015-08-23 10:33:45 +0800

    INTERCEPTOR

--- src/main/java/echo/sp/app/command/interceptors/LoginInterceptor.java
+++ src/main/java/echo/sp/app/command/interceptors/LoginInterceptor.java
@@ -6,29 +6,67 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import echo.sp.app.command.model.SecCode;
+import echo.sp.app.command.page.PubTool;
+
 /**   
+ * INTERCEPTOR
  * @author Ethan   
  * @date 2015Äê8ÔÂ10ÈÕ 
  */
 public class LoginInterceptor extends HandlerInterceptorAdapter{
-	private static final String[] IGNORE_URI = {"/index.do","/search.do","paginator.do","paginatorParm.do"};
+	
+	// GET NORML CODE URI.
+	private static String NORMAL_URI = "/getCode.do";
+	
+	// NORMAL ACCESS WITHOUT AN USER LOGIN STATUS.
+	private static final String[] IGNORE_URI = {"/regist.do","/registalg.do","login.do"};
+	
+	// SECRET ACCESS NEED AN USER LOGIN STATUS.
+	private static final String[] SECRET_URI = {};
 	 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        boolean flag = false;
-        String url = request.getRequestURL().toString();
-        System.out.println(">>>: " + url);
-        System.out.println(">>>: " + url);
-        for (String s : IGNORE_URI) {
-            if (url.contains(s)) {
-                flag = true;
-                break;
-            }
-        }
+    	
+		boolean flag = false;
+		boolean find = false;
+		
+		String url = request.getRequestURL().toString();
+		String no_co = PubTool.processParm(request.getParameter("no_co"));
+		String sc_co = PubTool.processParm(request.getParameter("sc_co"));
+		
+		if (url.indexOf(NORMAL_URI) > 0) { // FIRST GET THE NORMAL CODE.
+			flag = true;
+		} else if (!"".equals(no_co) && !"".equals(sc_co)) { // SECRET ACCESS.
+			for (String s : SECRET_URI) {
+				if (url.contains(s)) {
+					find = true;
+					break;
+				}
+			}
+			// GET USER FROM SESSION, COMPARE THE SECRET CODE.
+			String user_id = (String) request.getSession().getAttribute("user_id");
+			if (find && SecCode.getKey(user_id).equals(sc_co)) {
+				flag = true;
+			}
+		}  else if (!"".equals(no_co)) {  // COMPARE THE NORMAL CODE.
+			for (String s : IGNORE_URI) { 
+				if (url.contains(s)) {
+					find = true;
+					break;
+				}
+			}
+			if (find && SecCode.getKey("NO_CO").equals(no_co)) {
+				flag = true;
+			}
+		}
+		
+		// IF NONE OF ABOVE EXISTS, PROCESS THIS ASK!
         if (!flag) {
             // T_supplier_user user = LoginController.getLoginUser(request);
             // if (user != null) flag = true;
         }
+        
         return flag;
     }
  
