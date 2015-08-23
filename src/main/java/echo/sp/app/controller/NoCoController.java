6896da0350247aa70ed97612bdc07f8e0e24b4a5
EthanProg <programcccethan@gmail.com>
2015-08-23 12:07:14 +0800

    TTTTTTTTTTTT

--- src/main/java/echo/sp/app/controller/NoCoController.java
+++ src/main/java/echo/sp/app/controller/NoCoController.java
@@ -42,6 +42,7 @@ public class NoCoController extends CoreController{
 			super.writeJson(response, Code.SUCCESS, Code.SUCCESS_MSG, resMap, null);
 		} else {
 			logger.error("NoCoController---getCode---bad no_co: " + no_co);
+			response.setStatus(401);
 		}
 	}
 }
