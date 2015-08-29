d7767ad6644e99792bdfcb03c456a1edd69be22a
EthanProg <programcccethan@gmail.com>
2015-08-29 11:28:33 +0800

    管理员

---
+++ src/main/java/echo/sp/app/controller/admin/AdminLoginController.java
@@ -0,0 +1,20 @@
+package echo.sp.app.controller.admin;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+import echo.sp.app.command.core.CoreController;
+
+/** 
+ * 管理员登录  
+ * @author Ethan   
+ * @date 2015年8月29日 
+ */
+@Controller
+public class AdminLoginController extends CoreController{
+	
+	private static final Logger logger = LoggerFactory.getLogger(AdminLoginController.class);
+	
+	
+}
