332426c0fa9a04485d143137c1805265e794dc97
EthanProg <programcccethan@gmail.com>
2015-09-07 16:44:00 +0800

    ttttt

--- src/main/java/echo/sp/app/service/MerStoreService.java
+++ src/main/java/echo/sp/app/service/MerStoreService.java
@@ -1,9 +1,13 @@
 package echo.sp.app.service;
+
+import java.util.Map;
+
 /**   
  * 商铺
  * @author Ethan   
  * @date 2015年8月29日 
  */
 public interface MerStoreService {
-
+	// GET MERCHANTS DETAIL INFORMATION
+	public Map getMerDetail(Map parmMap);
 }
--- src/main/java/echo/sp/app/service/impl/MerStoreServiceImpl.java
+++ src/main/java/echo/sp/app/service/impl/MerStoreServiceImpl.java
@@ -1,11 +1,13 @@
 package echo.sp.app.service.impl;
 
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import echo.sp.app.dao.MerItemDAO;
+import echo.sp.app.dao.MerStoreDAO;
 import echo.sp.app.service.MerStoreService;
 
 /**   
@@ -19,5 +21,11 @@ public class MerStoreServiceImpl implements MerStoreService {
 	private static final Logger logger = LoggerFactory.getLogger(MerStoreServiceImpl.class);
 	
 	@Autowired
-	private MerItemDAO merItemDAO;
+	private MerStoreDAO merStoreDAO;
+	
+	// GET MERCHANTS DETAIL INFORMATION
+	@Override
+	public Map getMerDetail(Map parmMap) {
+		return merStoreDAO.getMerDetail(parmMap);
+	}
 }
