446992b326f79ded12aec34b3e82aa18e67b982b
EthanProg <programcccethan@gmail.com>
2015-09-08 08:22:10 +0800

    BUG修正

--- src/main/java/echo/sp/app/controller/pub/LoginController.java
+++ src/main/java/echo/sp/app/controller/pub/LoginController.java
@@ -310,14 +310,16 @@ public class LoginController extends CoreController{
 		
 		// Get User Base Information
 		resMap = userService.getUserInfo(parmMap);
+		
 		// Get Merchant Base Information
 		Map merMap = userService.getMerchantInfo(parmMap);
-		
-		resMap.putAll(merMap);    
-		
+		if (merMap != null) {
+			resMap.putAll(merMap);
+			session.setAttribute("MERCHANT_ID", resMap.get("MERCHANT_ID").toString());
+		}
+		    
 		session.setAttribute("user_id", user_id);
 		session.setAttribute("CANT_CODE", resMap.get("CANT_CODE"));// District Number
-		session.setAttribute("MERCHANT_ID", resMap.get("MERCHANT_ID").toString());
 		
 		return resMap;
 	}
