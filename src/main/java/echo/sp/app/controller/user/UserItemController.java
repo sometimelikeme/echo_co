c49a562663e37067d24168804b1776641c95a7d6
EthanProg <programcccethan@gmail.com>
2015-09-14 11:32:54 +0800

    T

--- src/main/java/echo/sp/app/controller/user/UserItemController.java
+++ src/main/java/echo/sp/app/controller/user/UserItemController.java
@@ -1,11 +1,20 @@
 package echo.sp.app.controller.user;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import echo.sp.app.command.core.CoreController;
+import echo.sp.app.command.model.Code;
+import echo.sp.app.command.utils.UserAgentUtils;
 import echo.sp.app.service.UserItemService;
 
 /**   
@@ -21,5 +30,155 @@ public class UserItemController extends CoreController{
 	@Autowired
 	private UserItemService userItemService;
 	
+	/**
+	 * 收藏店铺
+	 * @param req
+	 * @param response
+	 * @param dataParm
+	 */
+	@RequestMapping("user/addMerColl")
+	public void addMerColl(HttpServletRequest req, HttpServletResponse response, @RequestParam String dataParm) {
+		if (logger.isDebugEnabled()) {
+			logger.debug("UserItemController---addMerColl---dataParm: " + dataParm);
+		}
+		
+		try {
+			super.getParm(req, response);
+			
+			Map paramMap = data.getDataset();
+			
+			String user_id = (String) paramMap.get("USER_ID"), 
+				   ut = (String) paramMap.get("ut"), 
+				   s_user_id = (String) session.getAttribute("user_id");
+			
+			// Get and compare with user id in session
+			if (user_id == null || (user_id != null && !user_id.equals(s_user_id))) {
+				super.writeJson(response, Code.FAIL, "无效用户！", null, null);
+			} else if (!"10".equals(ut)) {// Only user has access
+				super.writeJson(response, "9998", "无效客户端", null, null);
+			} else if (!UserAgentUtils.isMobileOrTablet(req)) {
+				super.writeJson(response, "9997", "无效设备", null, null);
+			} else {
+				userItemService.addMerColl(paramMap);
+				super.writeJson(response, Code.SUCCESS, Code.SUCCESS_MSG, null, null);
+			}
+		} catch (Exception e) {
+			super.writeJson(response, "9992", "后台程序执行失败", null, null);
+			logger.error("UserItemController---addMerColl---interface error: ", e);
+		}
+	}
 	
+	/**
+	 * 收藏商品
+	 * @param req
+	 * @param response
+	 * @param dataParm
+	 */
+	@RequestMapping("user/addItemColl")
+	public void addItemColl(HttpServletRequest req, HttpServletResponse response, @RequestParam String dataParm) {
+		if (logger.isDebugEnabled()) {
+			logger.debug("UserItemController---addItemColl---dataParm: " + dataParm);
+		}
+		
+		try {
+			super.getParm(req, response);
+			
+			Map paramMap = data.getDataset();
+			
+			String user_id = (String) paramMap.get("USER_ID"), 
+				   ut = (String) paramMap.get("ut"), 
+				   s_user_id = (String) session.getAttribute("user_id");
+			
+			// Get and compare with user id in session
+			if (user_id == null || (user_id != null && !user_id.equals(s_user_id))) {
+				super.writeJson(response, Code.FAIL, "无效用户！", null, null);
+			} else if (!"10".equals(ut)) {// Only user has access
+				super.writeJson(response, "9998", "无效客户端", null, null);
+			} else if (!UserAgentUtils.isMobileOrTablet(req)) {
+				super.writeJson(response, "9997", "无效设备", null, null);
+			} else {
+				userItemService.addItemColl(paramMap);
+				super.writeJson(response, Code.SUCCESS, Code.SUCCESS_MSG, null, null);
+			}
+		} catch (Exception e) {
+			super.writeJson(response, "9992", "后台程序执行失败", null, null);
+			logger.error("UserItemController---addItemColl---interface error: ", e);
+		}
+	}
+	
+	/**
+	 * 删除收藏店铺
+	 * @param req
+	 * @param response
+	 * @param dataParm
+	 */
+	@RequestMapping("user/deleteMerColl")
+	public void deleteMerColl(HttpServletRequest req, HttpServletResponse response, @RequestParam String dataParm) {
+		if (logger.isDebugEnabled()) {
+			logger.debug("UserItemController---deleteMerColl---dataParm: " + dataParm);
+		}
+		
+		try {
+			super.getParm(req, response);
+			
+			Map paramMap = data.getDataset();
+			
+			String user_id = (String) paramMap.get("USER_ID"), 
+				   ut = (String) paramMap.get("ut"), 
+				   s_user_id = (String) session.getAttribute("user_id");
+			
+			// Get and compare with user id in session
+			if (user_id == null || (user_id != null && !user_id.equals(s_user_id))) {
+				super.writeJson(response, Code.FAIL, "无效用户！", null, null);
+			} else if (!"10".equals(ut)) {// Only user has access
+				super.writeJson(response, "9998", "无效客户端", null, null);
+			} else if (!UserAgentUtils.isMobileOrTablet(req)) {
+				super.writeJson(response, "9997", "无效设备", null, null);
+			} else {
+				userItemService.deleteMerColl(paramMap);
+				super.writeJson(response, Code.SUCCESS, Code.SUCCESS_MSG, null, null);
+			}
+		} catch (Exception e) {
+			super.writeJson(response, "9992", "后台程序执行失败", null, null);
+			logger.error("UserItemController---deleteMerColl---interface error: ", e);
+		}
+	}
+	
+	/**
+	 * 删除收藏商品
+	 * @param req
+	 * @param response
+	 * @param dataParm
+	 */
+	@RequestMapping("user/deleteItemColl")
+	public void deleteItemColl(HttpServletRequest req, HttpServletResponse response, @RequestParam String dataParm) {
+		if (logger.isDebugEnabled()) {
+			logger.debug("UserItemController---deleteItemColl---dataParm: " + dataParm);
+		}
+		
+		try {
+			super.getParm(req, response);
+			
+			Map paramMap = data.getDataset();
+			
+			String user_id = (String) paramMap.get("USER_ID"), 
+				   ut = (String) paramMap.get("ut"), 
+				   s_user_id = (String) session.getAttribute("user_id");
+			
+			// Get and compare with user id in session
+			if (user_id == null || (user_id != null && !user_id.equals(s_user_id))) {
+				super.writeJson(response, Code.FAIL, "无效用户！", null, null);
+			} else if (!"10".equals(ut)) {// Only user has access
+				super.writeJson(response, "9998", "无效客户端", null, null);
+			} else if (!UserAgentUtils.isMobileOrTablet(req)) {
+				super.writeJson(response, "9997", "无效设备", null, null);
+			} else {
+				userItemService.deleteMerColl(paramMap);
+				super.writeJson(response, Code.SUCCESS, Code.SUCCESS_MSG, null, null);
+			}
+		} catch (Exception e) {
+			super.writeJson(response, "9992", "后台程序执行失败", null, null);
+			logger.error("UserItemController---deleteItemColl---interface error: ", e);
+		}
+	}
 }
