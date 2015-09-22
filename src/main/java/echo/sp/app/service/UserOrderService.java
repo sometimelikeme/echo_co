126f2060154c775dd97c586886b2293bb5cfd1f3
EthanProg <programcccethan@gmail.com>
2015-09-22 15:29:21 +0800

    T

--- src/main/java/echo/sp/app/service/UserOrderService.java
+++ src/main/java/echo/sp/app/service/UserOrderService.java
@@ -1,9 +1,13 @@
 package echo.sp.app.service;
+
+import java.util.Map;
+
 /**   
  * 用户订单 
  * @author Ethan   
  * @date 2015年8月29日 
  */
 public interface UserOrderService {
-
+	// Make Order
+	public int addOrder(Map parmMap);
 }
