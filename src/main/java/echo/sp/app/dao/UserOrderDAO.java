02393d010347dd1079ebdbfd176aff4faac9f212
EthanProg <programcccethan@gmail.com>
2015-09-22 15:29:49 +0800

    t

--- src/main/java/echo/sp/app/dao/UserOrderDAO.java
+++ src/main/java/echo/sp/app/dao/UserOrderDAO.java
@@ -1,9 +1,16 @@
 package echo.sp.app.dao;
+
+import java.util.List;
+import java.util.Map;
+
 /** 
  * 用户订单  
  * @author Ethan   
  * @date 2015年8月29日 
  */
 public interface UserOrderDAO {
-
+	// Make Order
+	public int addOrderHead(Map parmMap);
+	// Make Order
+	public int addOrderLine(List list);
 }
--- src/main/java/echo/sp/app/service/impl/UserOrderServiceImpl.java
+++ src/main/java/echo/sp/app/service/impl/UserOrderServiceImpl.java
@@ -1,5 +1,8 @@
 package echo.sp.app.service.impl;
 
+import java.util.List;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,4 +23,20 @@ public class UserOrderServiceImpl implements UserOrderService{
 	
 	@Autowired
     private UserOrderDAO userOrderDAO;
+
+	@Override
+	public int addOrder(Map parmMap) {
+		int returnInt = 0;
+    	try {
+    		if (userOrderDAO.addOrderHead((Map)parmMap.get("head")) > 0) {
+    			if (userOrderDAO.addOrderLine((List)parmMap.get("line")) > 0) {
+    				returnInt = 1;
+    			};
+    		}
+		} catch (Exception e) {
+			//  默认spring事务只在发生未被捕获的 runtimeexcetpion时才回滚
+			throw new RuntimeException();
+		}
+		return returnInt;
+	}
 }
