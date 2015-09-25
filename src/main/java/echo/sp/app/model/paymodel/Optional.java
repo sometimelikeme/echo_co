f68f82e553107f1390488409ccc955fb32d66a65
EthanProg <programcccethan@gmail.com>
2015-09-25 15:42:33 +0800

    T

--- src/main/java/echo/sp/app/model/paymodel/Optional.java
+++ src/main/java/echo/sp/app/model/paymodel/Optional.java
@@ -5,5 +5,25 @@ package echo.sp.app.model.paymodel;
  * @date 2015年9月25日 
  */
 public class Optional {
-
+	String ORDER_ID = "";// 订单号
+	String RANDOM_ID = "";// 随机号
+	String SECTION = "";// 当前随机号所在序列:支持枚举1,2
+	public String getORDER_ID() {
+		return ORDER_ID;
+	}
+	public void setORDER_ID(String oRDER_ID) {
+		ORDER_ID = oRDER_ID;
+	}
+	public String getRANDOM_ID() {
+		return RANDOM_ID;
+	}
+	public void setRANDOM_ID(String rANDOM_ID) {
+		RANDOM_ID = rANDOM_ID;
+	}
+	public String getSECTION() {
+		return SECTION;
+	}
+	public void setSECTION(String sECTION) {
+		SECTION = sECTION;
+	}
 }
