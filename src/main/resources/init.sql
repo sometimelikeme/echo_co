2b2ef388c76dc4b9c37ff9423c89e1b419857a09
EthanProg <programcccethan@gmail.com>
2015-10-03 18:20:40 +0800

    T

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -37,5 +37,7 @@ INSERT INTO T_ORG_PARM VALUES('120000','MER_ITEM_CHECK','0','店铺上传商品�
 INSERT INTO T_ORG_PARM VALUES('120000','MER_ITEM_MAX_QTY','5','店铺上传商品店铺上传商品的最大数量,默认无限制','1');
 --按照订单价格百分比返还积分
 INSERT INTO T_ORG_PARM VALUES('120000','PER_POINT_AWARD_ORDER','5','按照订单价格奖励积分的百分比','1');
+--用户发布任务是否需要审核
+INSERT INTO T_ORG_PARM VALUES('120000','USER_UP_TASK_CHECK','0','用户发布任务是否需要审核：1-是，0-否（默认）','1');
 
 
